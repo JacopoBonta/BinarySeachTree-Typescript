557e94a2096768212f5bdf6d2e44017fb09a0f92
Jacopo Bontà <bonta.jacopo@gmail.com>
2017-09-28 22:47:46 +0200

    code reviw

--- main.ts
+++ main.ts
@@ -20,24 +20,3 @@ console.log("Print tree in ascendent order");
 tree.inOrderTraverse((node :number) => console.log(node));
 console.log("Print tree in descendent order");
 tree.preOrderTraverse((node :number) => console.log(node));
-
-
-/* console.log("Is empty? ", tree.isEmpty());
-console.log("Cardinality ", tree.cardinality());
-console.log("There is a member with value of 5? ", tree.member(5));
-console.log("Adding a new member with value of 2... ", tree = tree.add(2));
-console.log("");
-console.log("Is empty? ", tree.isEmpty());
-console.log("Cardinality ", tree.cardinality());
-console.log("There is a member with value of 5? ", tree.member(5));
-console.log("Adding a new member with value of 3... ", tree = tree.add(3));
-console.log("");
-console.log("Is empty? ", tree.isEmpty());
-console.log("Cardinality ", tree.cardinality());
-console.log("There is a member with value of 5? ", tree.member(5));
-console.log("Adding a new member with value of 5... ", tree = tree.add(5));
-console.log("");
-console.log("Is empty? ", tree.isEmpty());
-console.log("Cardinality ", tree.cardinality());
-console.log("There is a member with value of 5? ", tree.member(5));
-console.log("Adding a new member with value of 7... ", tree = tree.add(7)); */
