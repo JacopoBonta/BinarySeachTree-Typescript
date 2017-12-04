5fb6212573545a9de7fdcb2bf75b0e471850915d
Jacopo Bontà <bonta.jacopo@gmail.com>
2017-12-04 11:40:32 +0100

    code review

--- main.ts
+++ main.ts
@@ -1,22 +1,22 @@
 import EmptyBST from "./EmptyBST"
 import NonEmptyBST from "./NonEmptyBST"
 
-let tree = new EmptyBST<number>();
-let size = 5;
+let tree = new EmptyBST<number>()
+let size = 5
 let sample = []
 
 while (size-->0) {
-    sample.push(Math.floor(Math.random() * 10));
+    sample.push(Math.floor(Math.random() * 10))
 }
 
-console.log("Sample array ", sample);
+console.log("Sample array ", sample)
 
-sample.forEach(elem => {
-    tree = tree.insert(elem);
-});
+sample.forEach(elem => tree = tree.insert(elem))
 
-console.log("Tree cardinality - ", tree.cardinality());
-console.log("Print tree in ascendent order");
-tree.inOrderTraverse((node :number) => console.log(node));
-console.log("Print tree in descendent order");
-tree.preOrderTraverse((node :number) => console.log(node));
+console.log("Tree cardinality - ", tree.cardinality())
+
+console.log("Print tree in ascendent order")
+tree.inOrderTraverse((node :number) => console.log(node))
+
+console.log("Print tree in descendent order")
+tree.preOrderTraverse((node :number) => console.log(node))
